291b51f7416174ddfb3bb25ddcb35d6021c1dc90
Chrissy1025 <67969110+Chrissy1025@users.noreply.github.com>
2020-07-14 00:54:26 -0500

    Add files via upload

---
+++ Christiana_L_OperatorExcercise.playground/Contents.swift
@@ -0,0 +1,22 @@
+import UIKit
+
+var str = "Hello, playground"
+
+//Define all input variables
+
+var a = 10
+var b = 7
+ 
+//List Operations
+
+let operation1 = a + b
+let operation2 = a - b
+let operation3 = a * b
+let operation4 = a / b
+
+//Print Results
+ 
+print(operation1)
+print(operation2)
+print(operation3)
+print(operation4)
